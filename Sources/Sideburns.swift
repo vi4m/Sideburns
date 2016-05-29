848f682ce9b2ca480f44c0a23e008f6d5c55d854
Marcin Kliks <marcin.kliks@allegro.pl>
2016-05-29 10:30:15 +0200

    initial

--- Sources/Sideburns.swift
+++ Sources/Sideburns.swift
@@ -34,20 +34,16 @@ public enum SideburnsError: ErrorProtocol {
 }
 
 extension Response {
-    public init(status: Status = .ok, headers: Headers = [:], templatePath: String, templateData: TemplateData) throws {
-        let templateFile = try File(path: templatePath, mode: .read)
+    public init(status: Status = .ok, headers: Headers = [:], templateName: String, repository: TemplateRepository, templateData: TemplateData) throws {
 
-        guard let templateString = try? String(data: templateFile.readAllBytes()) else {
-            throw SideburnsError.unsupportedTemplateEncoding
-        }
-
-        let template = try Template(string: templateString)
+        let template = try repository.template(named: templateName)
+        
         let rendering = try template.render(box: Box(boxable: templateData))
 
         self.init(status: status, headers: headers, body: rendering)
 
-        if let fileExtension = templateFile.fileExtension, mediaType = mediaType(forFileExtension: fileExtension) {
-            self.contentType = mediaType
-        }
+//        if let fileExtension = templateFile.fileExtension, mediaType = mediaType(forFileExtension: fileExtension) {
+//            self.contentType = mediaType
+//        }
     }
 }
